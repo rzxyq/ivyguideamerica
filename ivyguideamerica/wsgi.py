2c51f658d440063ecda5d6da2758c9cbb1931a6d
Ruoyan Qin <rq25@cornell.edu>
2016-02-04 12:14:38 -0500

    Modified wsgi.py

--- ivyguideamerica/wsgi.py
+++ ivyguideamerica/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ivyguideamerica.settings")
+
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ivyguideamerica.settings")
 
-application = get_wsgi_application()
+
+# Django Heroku
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
